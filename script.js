cf21732ae3f1c94bba938a623317d41ff3ffb88c
BeqaMir <146536309+BeqaMir@users.noreply.github.com>
2023-11-10 16:34:19 +0400

    create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+var name = "Beqa";
+var lastName ="Mirianashvili";
+var istrue = "true";
+const person = {birhday: "1997",};
+
+
+var hight = 7;
+var width = 5;
+var area;
+area = width * hight;
+console.log(`martkutxedis fartobi = ${area}`);
+
+
+var a = 5;
+var b = 7;
+var c;
+c=a+b;
+var b;
+b=c-a;
+console.log(c);
+console.log(b);
+var x = 2;
+var y = 3;
+var z;
+z=x*y;
+var x;
+x=z/y;
+console.log(z);
+console.log(x);
+
+
+console.log("where are you living :" + " " + living);
